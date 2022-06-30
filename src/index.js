a78e9496cc783bc8b0344665b055bc280180e684
German Gonzalez <german.gonzalez@wizeline.com>
2022-06-30 13:25:08 -0500

    first config.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MoviesList from './imdb/MoviesList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoviesList />
   </React.StrictMode>
 );
 
